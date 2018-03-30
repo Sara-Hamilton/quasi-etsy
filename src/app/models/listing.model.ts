9b9ed6d1359f5e0dd449c746155950c1f8de55f9
Sara Hamilton <sara@indeterminate.net>
2018-03-30 09:45:42 -0700

    add category to listing constructor

--- src/app/models/listing.model.ts
+++ src/app/models/listing.model.ts
@@ -1,9 +1,10 @@
 import { User } from './user.model';
 import { Feedback } from './feedback.model';
+import { Category } from './category.model';
 
 export class Listing {
   public feedback: Feedback[] = [];
   public favoritedBy: User[] = [];
 
-  constructor(public image: string, public description: string, public price: number, public shippingTime: string) {}
+  constructor(public image: string, public description: string, public category: Category, public price: number, public shippingTime: string) {}
 }
