d06a14a9ebc8f8f68f75d46bbbc3ea1d7ed22495
Sara Hamilton <sara@indeterminate.net>
2018-03-30 10:10:29 -0700

    add new-user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule
