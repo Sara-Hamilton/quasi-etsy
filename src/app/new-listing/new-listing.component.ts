2ef08d21d22542115b59b18ad09878c0db1530d4
Sara Hamilton <sara@indeterminate.net>
2018-03-30 14:12:23 -0700

    add input, output, form input, and new listing constructor to new-listing.component.ts

--- src/app/new-listing/new-listing.component.ts
+++ src/app/new-listing/new-listing.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-new-listing',
   templateUrl: './new-listing.component.html',
   styleUrls: ['./new-listing.component.css']
 })
-export class NewListingComponent implements OnInit {
+export class NewListingComponent {
+  @Input() childCategoryList: Category[];
+  @Output() sendListing = new EventEmitter();
 
-  constructor() { }
+  submitListingForm(image: string, description: string, category: string, price: string, shippingTime: string) {
+    let newCategory: Category = new Category(category);
+    let newListing: Listing = new Listing (image, description, newCategory, parseInt(price), shippingTime);
 
-  ngOnInit() {
   }
-
 }
