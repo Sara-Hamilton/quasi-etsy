ec11b22ed49b4d418f377ae1444bf17cc38c24f5
Sara Hamilton <sara@indeterminate.net>
2018-03-30 10:19:59 -0700

    add form submission and event emitter to new-user component

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
   styleUrls: ['./new-user.component.css']
 })
-export class NewUserComponent implements OnInit {
+export class NewUserComponent {
+  @Output() sendUser = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  submitForm(name: string, email: string, city: string, state: string, joinDate: Date) {
+    let newUser: User = new User(name, email, city, state, joinDate);
+    this.sendUser.emit(newUser);
   }
 
 }
