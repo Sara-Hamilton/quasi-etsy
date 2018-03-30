1e881048fa31561f9d20a1d2f546a0aeee29ce81
Sara Hamilton <sara@indeterminate.net>
2018-03-30 11:11:33 -0700

    add comment to new-shop component, unsure how to get current user to add to shop constructor

--- src/app/new-shop/new-shop.component.ts
+++ src/app/new-shop/new-shop.component.ts
@@ -12,6 +12,7 @@ export class NewShopComponent {
 
   currentDate: Date = new Date();
 
+  // owner should be a User object - will be current logged in user - unsure how to get user
   submitShopForm(owner: User, name: string, joinDate: Date) {
     let newShop: Shop = new Shop(owner, name, this.currentDate);
   }
