c076ce448ddd0af968c93fae4b66dd0efe91eef9
Sara Hamilton <sara@indeterminate.net>
2018-03-30 11:21:53 -0700

    create master user list in app.component to use to assign user to shop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,13 @@ import { User } from './models/user.model';
 })
 export class AppComponent {
   title = 'Quasi Etsy';
+  currentDate = new Date();
+
+  masterUserList: User[] = [
+    new User('Jenna', 'jenna@example.com', 'Portland', 'OR', this.currentDate),
+    new User('Steve', 'steve@example.com', 'Albuquerque', 'NM', this.currentDate),
+    new User('Kris', 'kris@example.com', 'Seattle', 'WA', this.currentDate),
+    new User('Lisa', 'lisa@example.com', 'New York', 'NY', this.currentDate),
+    new User('Charlie', 'charlie@example.com', 'Los Angeles', 'CA', this.currentDate),
+  ]
 }
