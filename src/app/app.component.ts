6fc7aa864d26c4559704180418e6000466370743
Sara Hamilton <sara@indeterminate.net>
2018-03-30 10:02:52 -0700

    import models into app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Category } from './models/category.model';
+import { Feedback } from './models/feedback.model';
+import { Listing } from './models/listing.model';
+import { Shop } from './models/shop.model';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
