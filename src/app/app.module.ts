398f464f49401d72f5babe5c63fb5e2a8dae8ff9
Sara Hamilton <sara@indeterminate.net>
2018-03-30 14:00:29 -0700

    add new-listing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NewUserComponent } from './new-user/new-user.component';
 import { NewShopComponent } from './new-shop/new-shop.component';
+import { NewListingComponent } from './new-listing/new-listing.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NewUserComponent,
-    NewShopComponent
+    NewShopComponent,
+    NewListingComponent
   ],
   imports: [
     BrowserModule
