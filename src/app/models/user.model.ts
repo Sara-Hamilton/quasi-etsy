73a71f8e6b59ae5742d855c9e3ef44d5cdf79425
Sara Hamilton <sara@indeterminate.net>
2018-03-30 09:01:43 -0700

    add constructor to User class

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,14 +1,8 @@
 export class User {
-  name
-  email
-  locationCity
-  locationState
-  joinDate
-  profile
-  favorites
-  shopOwner
-
-
-
-
+  public shopOwner: boolean = false;
+  public profile: string = "";
+  public favoriteShops: Shop[] = [];
+  public favoriteItems: Listing[] = [];
+  public picture: string = "";
+  constructor(public name: string, public email: string, public city: string, public state: string, public joinDate: Date) {}
 }
