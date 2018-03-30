728dbc21867ff7611374c58695b63333a9ad5f7c
Sara Hamilton <sara@indeterminate.net>
2018-03-30 13:41:19 -0700

    attempt to use hidden input on new-shop.html - not successful

--- src/app/new-shop/new-shop.component.ts
+++ src/app/new-shop/new-shop.component.ts
@@ -14,8 +14,12 @@ export class NewShopComponent {
   currentDate: Date = new Date();
 
   // TODO make owner in put a User object instead of a string
-  submitShopForm(owner: string, name: string, ){
-    let newShop: Shop = new Shop(owner, name, this.currentDate);
+  submitShopForm(owner: string, name: string, city: string){
+    console.log("owner", owner);
+    console.log("name", name);
+    console.log("city", city);
+    let newUser = new User(owner, "filler@example.com", "")
+    // let newShop: Shop = new Shop(owner, name, this.currentDate);
   }
 
 }
