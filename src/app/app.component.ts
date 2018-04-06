be1f8b21a945d877f9ec9313f023301312fb7f8d
Sara Hamilton <sara@indeterminate.net>
2018-04-06 16:24:00 -0700

    remove ids from models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,18 +13,18 @@ export class AppComponent {
   title = 'Quasi Etsy';
 
   masterUserList: User[] = [
-    new User('Jenna', 'jenna@example.com', 'Portland', 'OR', '01-12-2010', '1'),
-    new User('Steve', 'steve@example.com', 'Albuquerque', 'NM', '05-03-2015', '2'),
-    new User('Kris', 'kris@example.com', 'Seattle', 'WA', '03-03-2018', '3'),
-    new User('Lisa', 'lisa@example.com', 'New York', 'NY', '04-23-2017', '4'),
-    new User('Charlie', 'charlie@example.com', 'Los Angeles', 'CA', '12-12-2012', '5')
+    new User('Jenna', 'jenna@example.com', 'Portland', 'OR', '01-12-2010'),
+    new User('Steve', 'steve@example.com', 'Albuquerque', 'NM', '05-03-2015'),
+    new User('Kris', 'kris@example.com', 'Seattle', 'WA', '03-03-2018'),
+    new User('Lisa', 'lisa@example.com', 'New York', 'NY', '04-23-2017'),
+    new User('Charlie', 'charlie@example.com', 'Los Angeles', 'CA', '12-12-2012')
   ]
 
   masterShopList: Shop[] = [
-    new Shop('1', "Jenna's Jams", '02-01-2014', '1'),
-    new Shop('2', "Vim & Vinegar", '04-27-2015', '2'),
-    new Shop('3', "Object Shop", '05-03-2000', '3'),
-    new Shop('4', "Urban Boutique", '02-04-2016', '4'),
+    new Shop('1', "Jenna's Jams", '02-01-2014'),
+    new Shop('2', "Vim & Vinegar", '04-27-2015'),
+    new Shop('3', "Object Shop", '05-03-2000'),
+    new Shop('4', "Urban Boutique", '02-04-2016'),
   ]
 
   addUser(newUser: User) {
@@ -33,9 +33,5 @@ export class AppComponent {
 
   addShop(newShop: Shop) {
     this.masterShopList.push(newShop);
-  }
-
-  addCategory(selectedListing, category) {
-    selectedListing.category = category;
   }
 }
--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -12,7 +12,7 @@ export class NewUserComponent {
   currentDate: Date = new Date();
 
   submitUserForm(name: string, email: string, city: string, state: string, joinDate: Date) {
-    let newUser: User = new User(name, email, city, state, '01-01-2001', '55');
+    let newUser: User = new User(name, email, city, state, '01-01-2001');
     this.sendUser.emit(newUser);
   }
 
